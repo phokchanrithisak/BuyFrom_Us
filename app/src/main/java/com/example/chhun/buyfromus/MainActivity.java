d835ae8c979a48287f757715667862f3b460e945
Chhunnnn <30552313+chhunnnn@users.noreply.github.com>
2017-12-02 13:04:10 +0700

    go

--- app/src/main/java/com/example/chhun/buyfromus/MainActivity.java
+++ app/src/main/java/com/example/chhun/buyfromus/MainActivity.java
@@ -5,20 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button main_cat_btn1, main_cat_btn2, main_cat_btn3, main_cat_btn4;
+    ImageButton main_cat_btn1, main_cat_btn2, main_cat_btn3, main_cat_btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        main_cat_btn1 = (Button) findViewById(R.id.main_cat_btn1);
-        main_cat_btn2 = (Button) findViewById(R.id.main_cat_btn2);
-        main_cat_btn3 = (Button) findViewById(R.id.main_cat_btn3);
-        main_cat_btn4 = (Button) findViewById(R.id.main_cat_btn4);
+        main_cat_btn1 = (ImageButton) findViewById(R.id.main_cat_btn1);
+        main_cat_btn2 = (ImageButton) findViewById(R.id.main_cat_btn2);
+        main_cat_btn3 = (ImageButton) findViewById(R.id.main_cat_btn3);
+        main_cat_btn4 = (ImageButton) findViewById(R.id.main_cat_btn4);
 
     }
 
